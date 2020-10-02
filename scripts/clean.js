c5060b9e529f53538c84c5ede89948110cbdeb93
HiroyukiYagihashi <h.yagihashi2424@icloud.com>
2020-10-02 20:15:26 +0900

    Fix clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,5 +3,7 @@ const path = require('path');
 
 const destPath = path.resolve(path.dirname(__filename), '../docs');
 
-sh.rm('-rf', `${destPath}/*`)
-
+sh.rm('-rf', `${destPath}/assets`);
+sh.rm('-rf', `${destPath}/css`);
+sh.rm('-rf', `${destPath}/js`);
+sh.rm('-rf', `${destPath}/index.html`);
